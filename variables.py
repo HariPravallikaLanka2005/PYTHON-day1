a80f480b884563a7efb221fa64b792227e683212
HariPravallikaLanka2005 <haripravallikaists27@gmail.com>
2025-07-03 21:27:43 +0530

    PYTHON

---
+++ variables.py
@@ -0,0 +1,51 @@
+"""
+A Variable is a container which holds some information
+Ways to create a Variable
+1. A Variable should always start with a capital letter(A_Z)
+2. A Variable can also start with a small letter(a-z)
+3. A Variable can start with an underscore(_)
+"Ways not to create a variable"
+1. variable should not start with a digit(number)
+2. Variables are not created using a keywords
+"""
+D = 123 #Variable Declaratoion taking a character using the assignment operator we are assigning the values
+#D = it is called as variable intialisation
+#D------it is called as variable
+
+r = 123
+print(r)
+
+
+dept = "ece"
+print(dept)
+
+D123 = 'hi'
+print(D123)
+
+
+_f = "hello"
+print(_f)
+
+fig = "pig" #good 
+print(fig)
+
+_123 = "ists"
+print(_123)
+
+_A = 123
+print(_A)
+
+ddd = "ddd"
+print(ddd)
+
+A123 = 789
+print(A123)
+
+#123 = 123--bad
+#print(123)
+
+A8=123
+print(A8)
+
+a9 = "jahnavi789"
+print(a9)
